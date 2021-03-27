71bb2427df8f28759823790cc21f3948e0c11d6e
LubabaSheikh <73037638+LubabaSheikh@users.noreply.github.com>
2021-03-27 13:03:27 -0600

    Add files via upload

--- edu/ucalgary/ensf409/FurnitureDb.java
+++ edu/ucalgary/ensf409/FurnitureDb.java
@@ -1,5 +1,6 @@
 /**
  @author 
+
  @version
  
  @since 1.0
@@ -99,5 +100,32 @@ public class FurnitureDb{
         catch(SQLException e){
             e.printStackTrace();
         }
+    }
+
+    public void insertNewChair (String idNumber, String type, String legs, String arms, String seat, String cushion, String price, String manufacturerId){
+        try 
+            {
+                // first create the query itself as a string, it looks the same as the sql
+                String query = "INSERT INTO teacher (ID, Type, Legs, Arms, Seat, Cushion, Price, ManuID) VALUES (?,?,?,?,?,?,?)";
+                PreparedStatement stmt = myConnect.prepareStatement(query); // send query to PreparedStatement
+
+                stmt.setString(1, idNumber);
+                stmt.setString(2, type);
+                stmt.setString(3, legs);
+                stmt.setString(4, arms);
+                stmt.setString(5, seat);
+                stmt.setString(5, cushion);
+                stmt.setString(5, price);
+                stmt.setString(5, manufacturerId);
+
+                int rowCount = stmt.executeUpdate(); // updating the database, we are creating a transaction is the data base which is gonna update the data base
+                //System.out.println("Rows affected: " + rowCount);
+                
+                stmt.close(); // good practice to close
+            }
+            catch(SQLException ex)
+            {
+                ex.printStackTrace();
+            } 
     }
 }
