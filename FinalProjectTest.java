99081d7c8a679873e045f94c8e88e5d506537c2b
Heidi Schaefer <75228264+hnschaefer@users.noreply.github.com>
2021-03-30 13:20:24 -0600

    Add files via upload

---
+++ FinalProjectTest.java
@@ -0,0 +1,34 @@
+/**
+ @author 
+
+ @version 1.0
+ 
+ @since 1.0
+*/
+
+package edu.ucalgary.ensf409;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.io.*;
+import java.util.*;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+
+public class FinalProjectTest {
+
+    //@Test
+
+//Pre-test and post-test processes
+
+  @Before
+  public void start() {
+    removeAllData(DIR);
+  }
+
+  @After
+  public void end() {
+    removeAllData(DIR);
+  }
+
+//Methods to perform common routines 
+}
