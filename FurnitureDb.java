2db88ed5274356ac6377b9b8de02b1edd28fb5ef
Heidi Schaefer <75228264+hnschaefer@users.noreply.github.com>
2021-04-01 11:51:55 -0600

    Added comments, altered manufacturer class
    Manufacturer class no longer requires connection to database

--- FurnitureDb.java
+++ FurnitureDb.java
@@ -2,9 +2,24 @@
  @author Heidi Schaefer <a 
     href = "mailto:heidi.schaefer@ucalgary.ca">heidi.schaefer@ucalgary.ca</a>
 
- @version 1.4
+ @version 1.5
  
  @since 1.0
+*/
+
+/* Class FurnitureDb creates a connection to the database "inventory"
+*  It has the following functionalities:
+*  Removal of furniture items from database
+*  Creation of lists providing desired furniture types, 
+*   (eg. list of all available ergonomic chairs)
+*  A counter for how many items of the customer desired type can be created 
+*   using various parts of used furniture
+*  Creation of a list of manufacturers that can provide the desired
+*   type of item
+*
+*  This class is to be used in conjunction with the classes UserIO,
+*   FileIO, and Main, in order to successfully fulfill customers' used
+*   furniture needs
 */
 
 package edu.ucalgary.ensf409;
@@ -14,6 +29,7 @@ import java.io.*;
 import java.util.*;
 
 public class FurnitureDb{
+    // Variables for connection with database
     public final String DBURL;
     public final String USERNAME;
     public final String PASSWORD;
@@ -39,8 +55,10 @@ public class FurnitureDb{
 
     // Removal of furniture items from database
     // To be used once items have been placed in order form
-    // And are no longer in inventory
+    //  and are no longer in inventory
     public void removeFurnitureFromInventory(String category, String id){
+        // Connection to database, and creation of SQL query to delete items
+        //  from database
         try{
             String query = "DELETE FROM inventory." + category + " WHERE ID = ?";
             PreparedStatement stmt = dbConnect.prepareStatement(query);
@@ -57,13 +75,15 @@ public class FurnitureDb{
         }
     }
 
-    // Creation of ArrayList furnitureList that contains Furniture objects
-    // with only desired category and desired type
+    // Returns ArrayList "furnitureList" that contains Furniture objects
+    //  with only customer's desired category and desired type
+    //  (eg. list of standing desks)
     public ArrayList<Furniture> furnitureFinder(String category, String type){
         category = category.toLowerCase();
         ArrayList<Furniture> furnitureList = new ArrayList<Furniture>();
         ArrayList<Boolean> components = new ArrayList<Boolean>();
 
+        // Connection to database, and creation of SQL query
         try{
             Statement stmt = dbConnect.createStatement();
             String query = "SELECT * FROM inventory." + category;
@@ -73,6 +93,7 @@ public class FurnitureDb{
 
             // Adding boolean values to components array list
             // Checks if "arms", "legs", etc. are available
+            // If available, result = true
             while(results.next()){
                 for(int i = 3; i < columns - 2; i++){
                     if(resultsMeta.getColumnTypeName(i).equals("Y")){
@@ -85,7 +106,8 @@ public class FurnitureDb{
 
                 // Adding Furniture objects to array list furnitureList
                 if(results.getString("Type").equals(type)){
-                    furnitureList.add(new Furniture(results.getString("ID"), results.getString("Type"), components, results.getInt("Price"), results.getString("ManuID")));
+                    furnitureList.add(new Furniture(results.getString("ID"), results.getString("Type"), 
+                    components, results.getInt("Price"), results.getString("ManuID")));
                 }
             }
             stmt.close();
@@ -98,15 +120,16 @@ public class FurnitureDb{
         return furnitureList;
     }
 
-    // Returns either desired quantity or quantity available
+    // Counter which returns either desired quantity or quantity available of desired 
+    //  furniture type and category 
     public int componentCounter(ArrayList<Furniture> furnitureList, int desiredQuant){
         int componentCount = furnitureList.get(0).components.size();
         int maybeLeast = 0;
         int availableQuant = 0;
 
         // Counting how many of each component are available
-        // Example: If 4 arms are available, but only 2 legs,
-        // availableQuant will update to 2
+        //  (eg. if 4 arms are available, but only 2 legs,
+        //  availableQuant will update to 2)
         for(int i = 0; i < furnitureList.size(); i++){
             for(int j = 0; j < componentCount; j++){
                 if(furnitureList.get(i).components.get(j) == true){
@@ -119,7 +142,7 @@ public class FurnitureDb{
         }
 
         // If there are more furniture items available than
-        // the customer desires, only return as many as they want
+        //  the customer desires, only return quantity they asked for
         if (desiredQuant < availableQuant){
             return desiredQuant;
         }
@@ -127,19 +150,46 @@ public class FurnitureDb{
         return availableQuant;
     }
 
-    // Creating sets of manufacturers that sell desired types of furniture
-    // Note: A set does not allow duplicates
+    // Return sets of manufacturers that sell desired types of furniture
+    // To be run at the beginning of the program, before any items are removed
+    //  from inventory, in order for clients to have contacts of manufacturers
+    //  who carry their desired furniture category, in case the quantity they
+    //  desire is not available
     public Set<Manufacturer> manufacturerSuggestion(String category){
-        Set<Manufacturer> manufacturerList = new Set<Manufacturer>();
+        Set<Manufacturer> manufacturerList = new HashSet<>();
+        Set<String> manuID = new HashSet<>();
+
+        // Connection to database, and creation of SQL query
+        // Adds manufacturer IDs to manuID list, from items
+        //  of customer's desired category
         try{
             Statement stmt = dbConnect.createStatement();
             String query = "SELECT * FROM inventory." + category;
             results = stmt.executeQuery(query);
 
             while(results.next()){
-                manufacturerList.add(new Manufacturer(category, results.getString("ManuID")));
+                manuID.add(results.getString("ManuID"));
             }
             stmt.close();
+        }
+
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        
+        // Creation of set of manufacturers with manuIDs obtained
+        //  from previous code segment
+        try{
+            Statement stmt = dbConnect.createStatement();
+            String query = "SELECT * FROM inventory.manufacturer";
+            results = stmt.executeQuery(query);
+
+            while(results.next()){
+                if (manuID.contains(results.getString("ManuID"))){
+                    manufacturerList.add(new Manufacturer(results.getString("ManuID"), 
+                    results.getString("Name"), results.getString("Phone"), results.getString("Province")));
+                }
+            }
         }
 
         catch(SQLException e){
