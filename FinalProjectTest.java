32d5eda30f13c8aabf9458ffe1646444fa48a8ba
Rajpreetgill <73036580+Rajpreetgill@users.noreply.github.com>
2021-04-04 02:03:34 -0600

    Add files via upload

--- FinalProjectTest.java
+++ FinalProjectTest.java
@@ -1,6 +1,5 @@
 /**
  @author 
-
  @version 1.0
  
  @since 1.0
@@ -14,21 +13,194 @@ import java.io.*;
 import java.util.*;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
-public class FinalProjectTest {
+import jdk.jfr.Timestamp;
 
-    //@Test
 
-//Pre-test and post-test processes
+public class FinalProjectTest()
+{
+    public void FinalProjectTest()
+    {
+    }
+/* The following method checks if the UserIO constructor
+* initializes the data members inside the Class Manufacturer.
+*/
+@Test
+public void checkUserIOConstructor()
+{
+    UserIO userObject= new UserIO("category", "type", 2);
+    String result = userObject.getCategory();
+    String expResult= "category";
+    assertEquals("The constructor failed to initialize a data member.", expResult, result);
+}
+/* The following method checks if the method called userInput
+* correctly throws an error if an invalid category/type is entered 
+* which does not exist in the database. 
+*/
+@Test(expected=IllegalArgumentException.class)
+public void checkUserInputMethod()
+{
+    UserIO userObject = new UserIO("shelf","wooden", 2);
+    String result = UserIO.userInput();
+}
+/* The following method checks if the Manufacturer constructor
+* initializes the data members inside the Class Manufacturer.
+*/
+@Test
+public void checkManufacturerConstructor()
+{
+    Manufacturer manuObject= new Manufacturer("1234", "name", "0988901234", "province")
+    String result = manuObject.getName();
+    String expResult= "name";
+    assertEquals("The constructor failed to initialize a data member.", expResult, result);
+}
+/* The following method checks if the Furniture constructor
+* initializes the data members inside the Class Furniture.
+*/
+@Test 
+public void checkFurnitureConstructor()
+{
+    ArrayList<Boolean> list = new ArrayList<Boolean>();
+    list.add(true);
+    Furniture furnitureObject= new Furniture("1234", "type", list, "120","12345");
+    String result = furnitureObject.getType();
+    String expResult= "type";
+    assertEquals("The constructor failed to initialize a data member.", expResult, result);
+}
+/* The following method checks if the FurnitureDb constructor 
+ * initializes the data members in the class properly.
+ */
+@Test
+public void checkFurnitureDbConstructor()
+{
+ FurnitureDb furnitureDbObject new FurnitureDb("1234", "username", "password");
+ String result = furnitureDbObject.getDburl();
+ String expResult = "1234";
+ assertEquals("The constructor failed to initialize a data member.", expResult, result);
+}
 
-  @Before
-  public void start() {
-    removeAllData(DIR);
-  }
+/* The following test called checkDatabaseConnection
+ * checks if a connection with the database has been sucessfully made
+ */
+@Test
+public void checkDatabaseConnection()
+{
 
-  @After
-  public void end() {
-    removeAllData(DIR);
-  }
+}
 
-//Methods to perform common routines 
+/* The following test called checkRemoveFurnitureFromInventory()
+ * checks if the method removeFurnitureFromInventory successfully removes
+ * the specified item from the database. 
+ */
+@Test 
+public void checkRemoveFurnitureFromInventory()
+{
+
+}
+/* The following test called checkFurnitureFinder()
+ * checks if the method FurnitureFinder() successfully selects
+ * the specified category and type of the furniture from the database. 
+ */
+@Test 
+public void checkFurnitureFinder()
+{
+
+}
+/* The following test called checkComponentCounter()
+ * checks if the method called componentCounter() successfully returns the
+ * quantity of a specified furniture which was asked for. 
+ */
+@Test public void checkComponentCounter()
+{
+
+}
+/* The following test called checkManufacturerSuggestion()
+ * checks if the method called manufacturerSuggestion successfully 
+ * returns the correct manufacturer. 
+ */
+@Test public void checkManufacturerSuggestion()
+{
+
+}
+
+// this test performs if the method called insertNewChair
+// actually inserts a new chair into the database 
+@Test 
+public void testAddingNewChairToDatabase()
+{
+        FunTestFile object = new FunTestFile(); 
+        object.insertNewChair("C1234", "cool", "Y", "Y", "Y", "Y", "120", "000");
+        String type = "cool"; 
+        //checking if this method actually created a new chair in the database 
+        String expResult= "false";
+        String result= "true";
+
+        try{
+            Statement myStmtOne= dbConnect.createStatement();
+            results = myStmtOne.executeQuery("SELECT type FROM chair WHERE type= " + type);
+        if((results.next()))
+        {
+            expResult="true";
+        }
+        myStmtOne.close();
+        }catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        assertEquals("The method called insertNewChair inside the class FunTestFile does not insert a new chair into the database.", expResult, result);
+}
+
+/* The following test called testBlankOrderFormMethod()
+ * checks if the method called blankOrderFrom successfully 
+ * creates a new file.
+ */
+@Test
+public testBlankOrderFormMethod()
+{
+    FileIO fileObject= new FileIO();
+    fileObject.blankOrderForm();
+    File file = new File("c:/pathOfTheCreatedFile"); // **REMEMBER TO PUT IN THE PATH**
+    assertTrue(file.exists());
+}
+/* The following test called testBlankOrderFormContents()
+ * checks if the method called blankOrderFrom successfully 
+ * produces the correct contents in the file. 
+ */
+@Test
+public testBlankOrderFormContents()
+{
+    FileIO fileObject= new FileIO();
+    fileObject.blankOrderForm();
+    File file = new File("c:/pathOfTheCreatedFile");
+    //gotta set value
+    if(file.exists())
+    {
+        //check the contents now 
+    }
+}
+/*@Test
+public void testBlankOrderFormMethod()
+{
+    FileIO fileObject= new FileIO();
+    fileObject.blankOrderForm();
+    String result = "false";
+    String file= "Blank Furniture Order Form.txt";
+    if(file.exists)
+    {
+        result="true";
+    }
+    String expResult = "true";
+    assertEquals("The method called blankOrderForm failed to create the file", expResult, result);
+} */
+
+
+/* The following test called testCompleteOrderFormMethod()
+ * checks if the method called completeOrderForm successfully 
+ * creates a new file.
+ */
+@Test
+public void testCompleteOrderFormMethod()
+{
+
+}
+
+
 }
