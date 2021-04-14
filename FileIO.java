f140c525db2c96457e39f0a9e4fd7516dd3c0d32
Heidi Schaefer <75228264+hnschaefer@users.noreply.github.com>
2021-04-13 22:15:48 -0600

    Finalized files!

--- FileIO.java
+++ FileIO.java
@@ -17,29 +17,34 @@ import java.io.*;
 
 public class FileIO{
 
-    /**
-     * Class FileIO consists of two methods called blankOrderForm() and completeOrderForm().
-     * blankOrderForm() creates an order form that is blank with no price and item IDs.
-     * completeOrderForm() creates a filled out order form with the category, type and quantity as well as the item IDs 
-     * of the furnitures and the total price
-     */
-    
+    /** Class FileIO consists of two methods called blankOrderForm() 
+    * and completeOrderForm(). It can create a blank order form, or a 
+    * complete order form.
+    * blankOrderForm() creates an order form that is blank with no price and 
+    * item IDs.
+    * completeOrderForm() creates a filled out order form with the category, 
+    * type and quantity as well as the item IDs of the furnitures and the 
+    * total price
+    */
     public FileIO()
     {
 
     }
 
-
-    /* blankOrderForm() creates an order form that is blank with no price and item IDs.
-        no return value
-        takes in no arguments
-     */
+    /** Class blankOrderForm creates an order form that is blank with no price 
+    * and item IDs.
+    * No return value
+    * Takes in no arguments
+    */
     public void blankOrderForm()
     {
-        StringBuffer form = new StringBuffer("Furniture Order Form\n" + "\nFacultyName:"+
-                            "\nContact:" +"\nDate:"+"\n"+"\nOriginal Request:"+"\n"+"\nItems Ordered"+
-                            "\n"+"\nTotalPrice:");
-        File newfile = new File("Blank Furniture Order Form.txt"); // created a File object called newfile
+        StringBuffer form = new StringBuffer("Furniture Order Form\n" + 
+                    "\nFacultyName:" + "\nContact:" + "\nDate:" + "\n" + 
+                    "\nOriginal Request:" + "\n" + "\nItems Ordered" + 
+                    "\n" + "\nTotalPrice:");
+
+        // Create a File object called newfile
+        File newfile = new File("Blank Furniture Order Form.txt");
         try
         {
             if (!newfile.exists()) 
@@ -52,30 +57,38 @@ public class FileIO{
         } 
         catch (IOException e)
         {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
     
-    /*  completeOrderForm() creates a filled out order form with the category, type and quantity as well as the item IDs 
-        of the furnitures and the total price
-        not return value
-        takes in arguments ArrayList<Furniture> list, int totalPrice, String type, String category, int originalQuant and int availableQuant
-     */
-    public void completeOrderForm(ArrayList<Furniture> list, int totalPrice, String type, String category, int originalQuant, int availableQuant){
+    /** Method completeOrderForm creates a filled out order form with the 
+    * category, type and quantity as well as the item IDs 
+    * of the furnitures and the total price.
+    * No return value
+    * Takes in arguments ArrayList<Furniture> list, int totalPrice, 
+    * String type, String category, int originalQuant and int availableQuant
+    */
+    public void completeOrderForm(ArrayList<Furniture> list, int totalPrice, 
+            String type, String category, int originalQuant, 
+            int availableQuant){
         String title = "Furniture Order Form";
-        String fcd = "\n" + "\nFacultyName:" + "\nContact:" +"\nDate:";
-        StringBuffer originalRequest = new StringBuffer("\n" + "Original Request: " + type + " " + category + ", " + originalQuant);
-        StringBuffer availableRequest = new StringBuffer("\n" + "Available: " + type + " " + category + ", " + availableQuant);
+        String fcd = "\n" + "\nFacultyName:" + "\nContact:" + "\nDate:";
+        StringBuffer originalRequest = new StringBuffer("\n" + 
+                "Original Request: " + type + " " + category + ", " + 
+                originalQuant);
+        StringBuffer availableRequest = new StringBuffer("\n" + 
+                "Available: " + type + " " + category + ", " + 
+                availableQuant);
         
-        String itemsOrdered = "\n" +"Items Ordered";
+        String itemsOrdered = "\n" + "Items Ordered";
         for (int i = 0; i < list.size(); i++)
         {
             itemsOrdered = itemsOrdered + "\n" + "ID: " + list.get(i).getId();
         }
 
-        File newfile = new File("Furniture Order Form.txt"); // created a File object called newfile
+        // Create a File object called newfile
+        File newfile = new File("Furniture Order Form.txt"); 
         try
         {
             if (!newfile.exists()) 
@@ -88,12 +101,11 @@ public class FileIO{
             pw.println(originalRequest);
             pw.println(availableRequest);
             pw.println(itemsOrdered);
-            pw.print("\n"+"Total Price: $" +totalPrice);
+            pw.print("\n" + "Total Price: $" + totalPrice);
             pw.close();
         } 
         catch (IOException e)
         {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
--- TestFile.java
+++ TestFile.java
@@ -9,10 +9,9 @@
  @since 1.0
 */
 
-/*
-class TestFile has 17 tests which tests various functions
-of the files including FurnitureDb, FileIO, Furniture, Main, Manufacturer
-as well as UserIO files.
+/** Class TestFile has 17 tests which tests various functions
+* of the files including FurnitureDb, FileIO, Furniture, Main, Manufacturer
+* as well as UserIO files.
 */
 
 package edu.ucalgary.ensf409;
@@ -25,16 +24,18 @@ import java.util.*;
 import jdk.jfr.Timestamp;
 
 public class TestFile {
+
     /** The following method checks if the UserIO constructor
     * initializes the data members inside the Class Manufacturer.
     */
     @Test
     public void checkUserIOConstructor()
     {
-        UserIO userObject= new UserIO("chair", "Mesh", "2");
+        UserIO userObject = new UserIO("chair", "Mesh", "2");
         String result = userObject.getCategory();
-        String expResult= "chair";
-        assertEquals("The constructor failed to initialize a data member.", expResult, result);
+        String expResult = "chair";
+        assertEquals("The constructor failed to initialize a data member.", 
+                expResult, result);
     }
 
     /**The following method checks if the category setter method
@@ -97,11 +98,14 @@ public class TestFile {
     @Test
     public void checkManufacturerConstructor()
     {
-        Manufacturer manuObject= new Manufacturer("1234", "name", "0988901234", "province");
+        Manufacturer manuObject = new Manufacturer("1234", "name", "0988901234", 
+                "province");
         String result = manuObject.getName();
-        String expResult= "name";
-        assertEquals("The constructor failed to initialize a data member.", expResult, result);
+        String expResult = "name";
+        assertEquals("The constructor failed to initialize a data member.", 
+                expResult, result);
     }
+
     /**The following method checks if the Furniture constructor
     * initializes the data members inside the Class Furniture.
     */
@@ -110,21 +114,26 @@ public class TestFile {
     {
         ArrayList<Boolean> list = new ArrayList<Boolean>();
         list.add(true);
-        Furniture furnitureObject= new Furniture("1234", "type", list, 120,"12345");
+        Furniture furnitureObject = new Furniture("1234", "type", list, 120, 
+                "12345");
         String result = furnitureObject.getType();
-        String expResult= "type";
-        assertEquals("The constructor failed to initialize a data member.", expResult, result);
+        String expResult = "type";
+        assertEquals("The constructor failed to initialize a data member.", 
+                expResult, result);
     }
+
     /**The following method checks if the FurnitureDb constructor 
      * initializes the data members in the class properly.
      */
     @Test
     public void checkFurnitureDbConstructor()
     {
-    FurnitureDb furnitureDbObject = new FurnitureDb("1234", "username", "password");
+    FurnitureDb furnitureDbObject = new FurnitureDb("1234", "username", 
+            "password");
     String result = furnitureDbObject.getDburl();
     String expResult = "1234";
-    assertEquals("The constructor failed to initialize a data member.", expResult, result);
+    assertEquals("The constructor failed to initialize a data member.", 
+            expResult, result);
     }
 
     /**The following test called checkDatabaseConnection
@@ -133,7 +142,8 @@ public class TestFile {
     @Test
     public void checkDatabaseConnection()
     {
-        FurnitureDb furnitureObject= new FurnitureDb("jdbc:mysql://localhost/inventory", "ENSF409", "ensf409"); // need to supply the parameters 
+        FurnitureDb furnitureObject= new FurnitureDb
+                ("jdbc:mysql://localhost/inventory", "scm", "ensf409");
         furnitureObject.initializeConnection();
         int expResult = 1;
         int result = 0;
@@ -163,16 +173,21 @@ public class TestFile {
     @Test 
     public void checkFurnitureFinder()
     {
-        FurnitureDb furnitureObject = new FurnitureDb("jdbc:mysql://localhost/inventory", "ENSF409", "ensf409");
+        FurnitureDb furnitureObject = new FurnitureDb
+                ("jdbc:mysql://localhost/inventory", "scm", "ensf409");
         furnitureObject.initializeConnection();
-        ArrayList<Furniture> furnitureList = furnitureObject.furnitureFinder("chair", "Mesh");
+        ArrayList<Furniture> furnitureList = 
+                furnitureObject.furnitureFinder("chair", "Mesh");
         Furniture furnitureList0 = null;
         for (int i = 0; i < furnitureList.size(); i++)
         {
             if (furnitureList.get(i).getId().equals("C0942"))
             {
-                furnitureList0 = new Furniture(furnitureList.get(i).getId(), furnitureList.get(i).getType(),furnitureList.get(i).getComponents(),
-                                                furnitureList.get(i).getPrice(), furnitureList.get(i).getManuId());
+                furnitureList0 = new Furniture(furnitureList.get(i).getId(), 
+                            furnitureList.get(i).getType(),
+                            furnitureList.get(i).getComponents(),
+                            furnitureList.get(i).getPrice(), 
+                            furnitureList.get(i).getManuId());
                 break;
             }
         }
@@ -183,9 +198,11 @@ public class TestFile {
         components1.add(true);
         components1.add(true);
 
-        Furniture expObject = new Furniture("C0942", "Mesh", components1, 175, "005");
+        Furniture expObject = new Furniture("C0942", "Mesh", components1, 175, 
+                "005");
         furnitureObject.close();
-        assertEquals("The expected output doesnot match the result output.", expObject.getId(), furnitureList0.getId());
+        assertEquals("The expected output doesnot match the result output.", 
+                expObject.getId(), furnitureList0.getId());
     }
 
     /**The following test called checkComponentCounter()
@@ -195,12 +212,15 @@ public class TestFile {
     @Test 
     public void checkComponentCounter()
     {
-        FurnitureDb furnitureObject = new FurnitureDb("jdbc:mysql://localhost/inventory", "ENSF409", "ensf409");
+        FurnitureDb furnitureObject = new FurnitureDb
+                ("jdbc:mysql://localhost/inventory", "scm", "ensf409");
         furnitureObject.initializeConnection();
-        ArrayList<Furniture> furnitureList = furnitureObject.furnitureFinder("chair", "Mesh");
+        ArrayList<Furniture> furnitureList = 
+                furnitureObject.furnitureFinder("chair", "Mesh");
         int counter = furnitureObject.componentCounter(furnitureList, 3);
         furnitureObject.close();
-        assertEquals("The method componentCounter does not return correct value.", 1, counter);
+        assertEquals("The method componentCounter does not return correct value.", 
+                1, counter);
     }
 
     /**The following test called testBlankOrderFormMethod()
@@ -220,18 +240,22 @@ public class TestFile {
     /**The following test called testBlankOrderFormContents()
      * checks if the method called blankOrderForm successfully 
      * produces the correct contents in the file. 
-     * The following test code was inspired from this website: https://javaconceptoftheday.com/compare-two-text-files-in-java/
+     * The following test code was inspired from this website: 
+     * https://javaconceptoftheday.com/compare-two-text-files-in-java/
      */
     @Test
     public void testBlankOrderFormContents() throws Exception
     {
-        FileIO fileObject= new FileIO();
+        FileIO fileObject = new FileIO();
         fileObject.blankOrderForm();
         
-        StringBuffer expectedForm = new StringBuffer("Furniture Order Form\n" + "\nFacultyName:"+
-                            "\nContact:" +"\nDate:"+"\n"+"\nOriginal Request:"+"\n"+"\nItems Ordered"+
-                            "\n"+"\nTotalPrice:");
-        File newfile = new File("Expected Blank Furniture Order Form.txt"); // created a File object called newfile
+        StringBuffer expectedForm = new StringBuffer("Furniture Order Form\n" + 
+                        "\nFacultyName:" + "\nContact:" + "\nDate:" + "\n" + 
+                        "\nOriginal Request:" + "\n" + "\nItems Ordered" +
+                        "\n" + "\nTotalPrice:");
+
+        // Create a File object called newfile
+        File newfile = new File("Expected Blank Furniture Order Form.txt"); 
         try
         {
             if (!newfile.exists()) 
@@ -243,12 +267,13 @@ public class TestFile {
             pw.close();
         } catch (IOException e)
         {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
-        BufferedReader reader1 = new BufferedReader(new FileReader("Blank Furniture Order Form.txt"));
-        BufferedReader reader2 = new BufferedReader(new FileReader("Expected Blank Furniture Order Form.txt"));
+        BufferedReader reader1 = new BufferedReader(new FileReader
+                ("Blank Furniture Order Form.txt"));
+        BufferedReader reader2 = new BufferedReader(new FileReader
+                ("Expected Blank Furniture Order Form.txt"));
         String l1 = reader1.readLine();
         String l2 = reader2.readLine();
         boolean areEqual = true;
@@ -258,41 +283,36 @@ public class TestFile {
             if(l1 == null || l2 == null)
             {
                 areEqual = false;
-                 
                 break;
             }
             else if(! l1.equalsIgnoreCase(l2))
             {
                 areEqual = false;
-                 
                 break;
             } 
             l1 = reader1.readLine();
-             
             l2 = reader2.readLine();
-             
             lineNum++;
         }
 
         reader1.close();
         reader2.close();
-        assertTrue("The file does not match the expected outcomes",areEqual);
+        assertTrue("The file does not match the expected outcomes", areEqual);
     }
-
-    
 
     /**The following test called testCompleteOrderFormMethod()
      * checks if the method called completeOrderForm successfully 
      * creates a new file.
-     * The following test code was inspired from this website: https://javaconceptoftheday.com/compare-two-text-files-in-java/
+     * The following test code was inspired from this website:
+     * https://javaconceptoftheday.com/compare-two-text-files-in-java/
      */
     @Test
     public void testCompleteOrderFormMethod() throws Exception
     {
-        FurnitureDb database= new FurnitureDb("jdbc:mysql://localhost/inventory", "ENSF409", 
-                    "ensf409");
+        FurnitureDb database = new FurnitureDb("jdbc:mysql://localhost/inventory", 
+        "scm", "ensf409");
         database.initializeConnection();
-        FileIO fileObject= new FileIO();
+        FileIO fileObject = new FileIO();
         String category = "chair";
         String type = "Mesh";
         int desiredQuant = 1;
@@ -316,14 +336,17 @@ public class TestFile {
         fileObject.completeOrderForm(cheapestList, tPrice, type, category, 
                                     originalQuant, desiredQuant);
     
-        StringBuffer expectedForm = new StringBuffer("Furniture Order Form\n" + "\nFacultyName:"+
-                                                    "\nContact:" +"\nDate:"+"\n"+"\nOriginal Request: "+ 
-                                                    type + " " + category + ", " + originalQuant + "\n" + 
-                                                    "\nAvailable: " + type + " " + category + ", " + availableQuant + 
-                                                    "\n" + "\nItems Ordered" + "\nID: C6748" + "\nID: C8138" + "\nID: C9890"+ 
-                                                    "\n" + "\nTotal Price: $" + totalPrice);
-                            
-        File newfile = new File("Expected Furniture Order Form.txt"); // created a File object called newfile
+        StringBuffer expectedForm = new StringBuffer
+                    ("Furniture Order Form\n" + "\nFacultyName:"+
+                    "\nContact:" +"\nDate:"+"\n"+"\nOriginal Request: "+ 
+                    type + " " + category + ", " + originalQuant + "\n" + 
+                    "\nAvailable: " + type + " " + category + ", " + 
+                    availableQuant + "\n" + "\nItems Ordered" + "\nID: C6748" +
+                    "\nID: C8138" + "\nID: C9890"+ "\n" + "\nTotal Price: $" + 
+                    totalPrice);
+        
+        // Create a File object called newfile
+        File newfile = new File("Expected Furniture Order Form.txt"); 
         try
         {
             if (!newfile.exists()) 
@@ -335,12 +358,13 @@ public class TestFile {
             pw.close();
         } catch (IOException e)
         {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
-        BufferedReader reader1 = new BufferedReader(new FileReader("Furniture Order Form.txt"));
-        BufferedReader reader2 = new BufferedReader(new FileReader("Expected Furniture Order Form.txt"));
+        BufferedReader reader1 = new BufferedReader(new FileReader
+                ("Furniture Order Form.txt"));
+        BufferedReader reader2 = new BufferedReader(new FileReader
+                ("Expected Furniture Order Form.txt"));
         String l1 = reader1.readLine();
         String l2 = reader2.readLine();
         boolean areEqual = true;
@@ -350,26 +374,23 @@ public class TestFile {
             if(l1 == null || l2 == null)
             {
                 areEqual = false;
-                 
                 break;
             }
             else if(! l1.equalsIgnoreCase(l2))
             {
                 areEqual = false;
-                 
                 break;
             } 
             l1 = reader1.readLine();
-             
             l2 = reader2.readLine();
-             
             lineNum++;
         }
                  
         reader1.close();
         reader2.close();
-        assertTrue("The file does not match the expected outcomes",areEqual);
+        assertTrue("The file does not match the expected outcomes", areEqual);
     }
+
     /**The following test called testRemoveFurnitureMethod()
      * checks if the method called RemoveFurnitureMethod() successfully 
      * removes a chair from the database.
@@ -378,14 +399,17 @@ public class TestFile {
     public void testRemoveFurnitureMethod()
     {
         {
-            FurnitureDb furnitureObject = new FurnitureDb("jdbc:mysql://localhost/inventory", "ENSF409", "ensf409");
+            FurnitureDb furnitureObject = new FurnitureDb
+                    ("jdbc:mysql://localhost/inventory", "scm", "ensf409");
             furnitureObject.initializeConnection();
             furnitureObject.removeFurnitureFromInventory("chair", "C9890");
             String expResult ="false";
             String result = "true";
             try{
-                Statement myStmtOne = furnitureObject.getDbconnect().createStatement();
-                ResultSet results = myStmtOne.executeQuery("SELECT id FROM chair WHERE id ='C9890'");
+                Statement myStmtOne = 
+                        furnitureObject.getDbconnect().createStatement();
+                ResultSet results = myStmtOne.executeQuery
+                        ("SELECT id FROM chair WHERE id ='C9890'");
                 if(!(results.next()))
                 {
                     expResult = "true";
@@ -394,9 +418,11 @@ public class TestFile {
             }catch (SQLException ex) {
                 ex.printStackTrace();
             }
-            assertEquals("The method does not remove the category passed with ID.", expResult, result);
+            assertEquals("The method does not remove the category passed with ID.",
+                     expResult, result);
         }
     }
+
     /**The following test called testAddFurnitureToInventory()
      * checks if the method called addFurnitureToInventory successfully 
      * adds back the removed chair from the previous method.
@@ -404,22 +430,26 @@ public class TestFile {
     @Test 
     public void testAddFurnitureToInventory()
     {
-            FurnitureDb furnitureObject = new FurnitureDb("jdbc:mysql://localhost/inventory", "ENSF409", "ensf409");
+            FurnitureDb furnitureObject = new FurnitureDb
+                    ("jdbc:mysql://localhost/inventory", "scm", "ensf409");
             furnitureObject.initializeConnection();
             ArrayList<Boolean> components1 = new ArrayList<Boolean>();
             components1.add(false);
             components1.add(true);
             components1.add(false);
             components1.add(true);
-            Furniture testObject = new Furniture("C9890", "Mesh", components1, 50, "003");
+            Furniture testObject = new Furniture("C9890", "Mesh", components1, 
+                    50, "003");
             furnitureObject.addFurnitureToInventory(testObject, "chair");
             String expResult= "true";
             String result= "false";
 
             try{
-                Statement myStmtOne = furnitureObject.getDbconnect().createStatement();
-                ResultSet results = myStmtOne.executeQuery("SELECT id FROM chair WHERE id ='C9890'");
-            if((results.next()))
+                Statement myStmtOne = 
+                        furnitureObject.getDbconnect().createStatement();
+                ResultSet results = myStmtOne.executeQuery
+                        ("SELECT id FROM chair WHERE id ='C9890'");
+            if(results.next())
             {
                 result="true";
             }
